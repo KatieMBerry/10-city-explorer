370c4ce8608630697f39f818b138c0a4842c1912
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-11-09 17:47:34 -0800

    added getMunge function

---
+++ data/utils.js
@@ -0,0 +1,14 @@
+function getMunge(geoData) {
+
+    const firstItem = geoData[0];
+    return {
+        formatted_query: firstItem.display_name,
+        latitude: firstItem.lat,
+        longitude: firstItem.lon
+    };
+}
+
+module.exports = {
+    getMunge
+};
+
