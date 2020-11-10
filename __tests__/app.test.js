f07d1f5423577fd7ad5b2f9f2d57d9dfebb57281
KATIE BERRY <KATIEBERRYM@GMAIL.COM>
2020-11-10 12:04:41 -0800

    added test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 
-const { execSync } = require('child_process');
-
-const fakeRequest = require('supertest');
 const { getMungedGeo } = require('../data/utils');
-const app = require('../lib/app');
-const client = require('../lib/client');
+
 
 describe('app routes', () => {
   describe('routes', () => {
@@ -32,27 +28,27 @@ describe('app routes', () => {
     //   return client.end(done);
     // });
 
-    test('returns a new location data structure', async () => {
+    test('returns a new location data structure', () => {
 
-      const geoData = {
-        "place_id": "282983083",
-        "licence": "https://locationiq.com/attribution",
-        "osm_type": "relation",
-        "osm_id": "186579",
-        "boundingbox": [
-          "45.432536",
-          "45.6528812",
-          "-122.8367489",
-          "-122.4720252"
+      const geoData = [{
+        'place_id': '282983083',
+        'licence': 'https://locationiq.com/attribution',
+        'osm_type': 'relation',
+        'osm_id': '186579',
+        'boundingbox': [
+          '45.432536',
+          '45.6528812',
+          '-122.8367489',
+          '-122.4720252'
         ],
-        "lat": "45.5202471",
-        "lon": "-122.6741949",
-        "display_name": "Portland, Multnomah County, Oregon, USA",
-        "class": "place",
-        "type": "city",
-        "importance": 0.75356571743377,
-        "icon": "https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png"
-      }
+        'lat': '45.5202471',
+        'lon': '-122.6741949',
+        'display_name': 'Portland, Multnomah County, Oregon, USA',
+        'class': 'place',
+        'type': 'city',
+        'importance': 0.75356571743377,
+        'icon': 'https://locationiq.org/static/images/mapicons/poi_place_city.p.20.png'
+      }];
 
       const expectation = {
         formatted_query: geoData[0].display_name,
